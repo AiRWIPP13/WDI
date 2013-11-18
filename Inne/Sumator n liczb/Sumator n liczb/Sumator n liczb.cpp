b4e42e8a6598b4d5c3245956850ad0f24296e91f
Mizyn <llukas1994@wp.pl>
2013-11-18 01:33:21 +0100

    Czzytelniejszy kod
    + 1 drobna poprawka

--- Inne/Sumator n liczb/Sumator n liczb/Sumator n liczb.cpp
+++ Inne/Sumator n liczb/Sumator n liczb/Sumator n liczb.cpp
@@ -1,42 +1,41 @@
 // Sumator n liczb.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
 //
-
 #include "stdafx.h"
 
-int r,mnoznik;
-
-int rzad(int n) {
-    r=10;
-    while (r>=n) {
-    r=r*10;
+int rzad(int n) {              //wylicza rzad
+    int r=10;
+    while (r<=n) {
+    r*=10;
     }
     r=r/10;
     return r;
 }
 
-int wart_ntej_liczb (int n){
-    mnoznik=1;
+int wart_ntej_liczb (int n, int r){                 //wartosc n-tej liczby
+    int mnoznik=1;
     while ((n/mnoznik*r)==1){
         mnoznik=mnoznik+1;
     }
     return mnoznik;
 }
 
-int main()
+
+int main()                                //main()
 {
-    int a,n,suma,wart;
-    suma=0;
-    scanf("%d", &n);
+    int a, n, wart, ord;
+    int suma=0;
+    scanf_s("%d", &n);
 
-
-    while (n=0) {
-        int ord=rzad(n);
-        int wart=wart_ntej_liczb(n);
-        suma=suma+wart;
+    while (n==0) {
+        ord=rzad(n);                            //r->ord->r w 2 funkcji
+        wart=wart_ntej_liczb(n,ord);
+        suma+=wart;
         n=n-wart*10^ord;
     }
-    printf("%d\n",suma);
-    printf("zrobione");
+    
+	printf("%d\n",suma);
+    printf("Zrobione\n");
+
+	system("PAUSE");
     return 0;
 }
-;p
